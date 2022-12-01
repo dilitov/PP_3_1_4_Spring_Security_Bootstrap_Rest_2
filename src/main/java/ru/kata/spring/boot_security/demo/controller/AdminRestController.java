8529a5d575694bb06b0bbfcc50a013b154a8c478
Dilitov <tfutka@gmail.com>
2022-12-01 19:35:20 +0300

    ver_1.0

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -0,0 +1,78 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RolesService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest")
+public class AdminRestController {
+
+    private final UserService userService;
+
+    private final RolesService rolesService;
+
+    public AdminRestController(UserService userService, RolesService rolesService) {
+        this.userService = userService;
+        this.rolesService = rolesService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> getAllUser () {
+        final List<User> users = userService.getAllUsers();
+
+        return users != null &&  !users.isEmpty()
+                ? new ResponseEntity<>(users, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getCurrentUser(Authentication auth) {
+        final User currentUser = (User) auth.getPrincipal();
+        return currentUser != null
+                ? new ResponseEntity<>(currentUser, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") int id) {
+        System.out.println(1);
+        final User userById = userService.getUserById(id);
+        return userById != null
+                ? new ResponseEntity<>(userById, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> addUser(@RequestBody User newUser) {
+        userService.addUser(newUser);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?>  deleteUser (@PathVariable("id") int id) {
+        final boolean deleted = userService.deleteUser(id);
+        User user = userService.getUserById(id);
+
+        return deleted
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+    @PatchMapping("/")
+    public ResponseEntity<User> editUser(@RequestBody User editUser) {
+        final boolean edited = userService.updateUser(editUser);
+
+        return edited
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+
+}
---
+++ src/main/resources/static/users.js
@@ -0,0 +1,225 @@
+// Заполнение страницы All users
+
+$(async function () {
+    await getAllUsers();
+    showCurrentUser();
+});
+
+const allUsersTable = $('#tableAllUsers');
+
+async function getAllUsers() {
+
+    fetch("http://localhost:8080/rest/")
+        .then(res => res.json())
+        .then(res => {
+            allUsersTable.empty()
+            res.forEach(user => {
+                let tableWithUsers = `$(
+                        <tr>
+                            <td>${user.id}</td>
+                            <td>${user.name}</td>
+                            <td>${user.surName}</td>
+                            <td>${user.age}</td>
+                            <td>${user.username}</td>
+                            <td>${user.roles.map(role => " " + role.role)}</td>
+                            <td>
+                                <button type="button" class="btn btn-info" data-bs-toggle="modal" id="buttonEdit"
+                                data-action="edit" data-bs-id="${user.id}" data-bs-target="#modalEdit">Edit</button>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" id="buttonDelete"
+                                 data-action="delete" data-bs-id="${user.id}" data-bs-target="#modalDelete">Delete</button>
+                            </td>
+                        </tr>)`;
+
+                allUsersTable.append(tableWithUsers);
+            })
+        })
+}
+
+
+const currentUserTable = $('#currentUserTable');
+
+async function showCurrentUser() {
+    currentUserTable.empty()
+    fetch("http://localhost:8080/rest/user")
+        .then(res => res.json())
+        .then(user => {
+            let data = `$(
+                        <tr>
+                            <td>${user.id}</td>
+                            <td>${user.name}</td>
+                            <td>${user.surName}</td>
+                            <td>${user.age}</td>
+                            <td>${user.username}</td>
+                            <td>${user.roles.map(role => " " + role.role)}</td>
+                        </tr>)`;
+            $('#currentUserTable').append(data)
+        })
+}
+
+// Создание нового пользователя
+const formForNewUser = document.getElementById("formNewUser")
+formForNewUser.addEventListener('submit', getFormValue);
+
+async function getFormValue(event) {
+    event.preventDefault();
+
+    //вытаскиваем роли из формы
+    const userFormAuthorities = formForNewUser.querySelector('#roles')
+    const currentRoles = userFormAuthorities.selectedOptions
+    let newUserRoles = [];
+
+    for (let i = 0; i < currentRoles.length; i++) {
+        console.log(i)
+        newUserRoles.push({
+            id: currentRoles.item(i).value,
+            name: currentRoles.item(i).text
+        })
+    }
+
+    // Отправляем запрос
+    fetch("http://localhost:8080/rest/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: formForNewUser.name.value,
+            surName: formForNewUser.surName.value,
+            age: formForNewUser.age.value,
+            userName: formForNewUser.userName.value,
+            password: formForNewUser.password.value,
+            active: formForNewUser.isActive.value,
+            roles: newUserRoles
+        })
+    })
+        .then(() => {
+            formForNewUser.reset();
+            getAllUsers();
+            $('#allUsers-tab').click();
+        })
+
+}
+
+// модули
+
+// Получение пользователя по ID
+async function getUser(id) {
+    let url = "http://localhost:8080/rest/" + id;
+    let response = await fetch(url);
+    return await response.json();
+}
+function extracted(id, form, selectRole) {
+    let user = getUser(id)
+        .then(user => {
+            form.id.value = user.id
+            form.name.value = user.name
+            form.surName.value = user.surName
+            form.age.value = user.age
+            form.password.value = user.password
+            form.userName.value = user.userName
+            form.isActive.value = user.active
+
+            for (let option of selectRole.options) {
+                for (let i = 0; i < user.roles.length; i++) {
+                    if (parseInt(option.value) === user.roles[i].id) {
+                        option.selected = true
+                    }
+                }
+            }
+        })
+}
+
+// Изменение пользователя
+$('#modalEdit').on('show.bs.modal', function (event) {
+
+    let userEditID = event.relatedTarget.getAttribute('data-bs-id')
+    let form = document.querySelector('#formEdit')
+    form.reset()
+    let selectRole = document.getElementById('editRoles')
+        // Заполнение формы
+    extracted(userEditID, form, selectRole)
+
+        // Отправка формы
+    form.addEventListener('submit', function (ev) {
+        ev.preventDefault()
+        // Считывание ролей
+        const userFormEditRole = document.querySelector('#editRoles')
+        const currentRoles = userFormEditRole.selectedOptions
+        let editRoles = [];
+
+        for (let i = 0; i < currentRoles.length; i++) {
+            editRoles.push({
+                id: currentRoles.item(i).value,
+                name: currentRoles.item(i).text
+            })
+        }
+        // Отправка запросса на изменение
+        fetch("http://localhost:8080/rest/", {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: form.id.value,
+                name: form.name.value,
+                surName: form.surName.value,
+                age: form.age.value,
+                userName: form.userName.value,
+                password: form.password.value,
+                active: form.isActive.value,
+                roles: editRoles
+
+            })
+
+        }).then(() => {
+            $("#modalEdit").modal("hide")
+            getAllUsers()
+        })
+    })
+})
+
+
+// Удаление пользователя
+$('#modalDelete').on('show.bs.modal', function (event) {
+
+    let userDeleteID = event.relatedTarget.getAttribute('data-bs-id')
+    let form = document.querySelector('#deleteForm')
+    form.reset()
+    let selectRole = document.getElementById('deleteRoles')
+    // Заполнение формы
+    extracted(userDeleteID, form, selectRole);
+
+    // Отправка формы
+    form.addEventListener('submit', function (ev) {
+        ev.preventDefault()
+        // Считывание ролей
+        const userFormEditRole = document.querySelector('#deleteRoles')
+        const currentRoles = userFormEditRole.selectedOptions
+        let deleteRoles = [];
+
+        for (let i = 0; i < currentRoles.length; i++) {
+            deleteRoles.push({
+                id: currentRoles.item(i).value,
+                name: currentRoles.item(i).text
+            })
+        }
+        // Отправка запросса на изменение
+        fetch("http://localhost:8080/rest/"+userDeleteID, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: form.id.value
+            })
+
+        }).then(() => {
+            $("#modalDelete").modal("hide")
+            getAllUsers()
+        })
+    })
+})
+
+
