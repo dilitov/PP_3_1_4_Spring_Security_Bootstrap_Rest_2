4a9afcee76a7a80aebddbb543ff7cda5cbae4bbb
Dilitov <tfutka@gmail.com>
2022-12-08 15:36:00 +0300

    finish_2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,10 +1,15 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 @Controller
-
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -13,5 +18,10 @@ public class UserController {
         this.userService = userService;
     }
 
-
+    @GetMapping
+    public String showUser(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
+        return "admin/info-user";
+    }
 }
